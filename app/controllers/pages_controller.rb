daca6f7754c53f1941498a1d0ce8b673de59ce6e
Kunmasin <oniyeabdullahi00@gmail.com>
2024-12-28 10:20:32 +0100

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  #jump to view
+  def home
+  end
+
+  #jump to view
+  def about
+  end
+
+
+  #jump to view
+  def contact
+  end
+
+
+  #jump to view
+  def works
+  end
+
+end
